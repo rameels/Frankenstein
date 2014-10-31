7bfb46b3a5a5afd83036372fdd2215afd80a0029
Rameel Sethi <rameel92@hotmail.com>
2014-10-30 21:27:03 -0400

    Wrote html templates

--- frank_oldversion/frank/urls.py
+++ frank_oldversion/frank/urls.py
@@ -7,6 +7,8 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'frank.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-    url(r'^frank/', include('frankapp.urls')),
-    url(r'^admin/', include(admin.site.urls)),
+    #url(r'^frank/', include('frankapp.urls')),
+    #url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', views.search, name='search')
+    url(r'^$', views.results, name='results')
 )
--- frankapp/views.py
+++ frankapp/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+def results(request, actor, role, event, day, time, crew, responsibility):
+    try:
+        poll = Poll.objects.get(pk=poll_id)
+    except Poll.DoesNotExist:
+        raise Http404
+    return render(request, 'polls/results.html', {'poll': poll})
