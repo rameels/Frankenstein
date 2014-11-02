7ba4dbde6570fda4f9e05f1bb831fbfe89bc93e5
Rameel Sethi <rameel92@hotmail.com>
2014-11-02 01:48:28 -0400

    Updated views.py

--- frankapp/views.py
+++ frankapp/views.py
@@ -7,11 +7,18 @@ def search(request):
 	return render(request, 'frankapp/search.html')
 
 def searchpeople(request):
-	if 'stage' in request.GET:
-		results = Stages.objects.filter(name=request.GET['stage'])
-	return render(request, 'frankapp/searchpeople.html', {'results_list': results})
+	if 'types' in request.GET:
+		types = request.GET['types'])
+		if 'actor' in types:
+			
+		if 'crew' in types:
+
+		if 'role' in types:
+
+
+	return render(request, 'frankapp/search/people.html', {'results_list': results})
 
 def searchevents(request):
 	if 'stage' in request.GET:
 		results = Stages.objects.filter(name=request.GET['stage'])
-	return render(request, 'frankapp/searchevents.html', {'results_list': results})
+	return render(request, 'frankapp/search/events.html', {'results_list': results})
