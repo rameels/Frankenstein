0b45d5bbdaef9c79a1ef09e0dbe7208d32bd7331
rsethi1 <rameel.sethi@gmail.com>
2015-06-22 03:48:18 -0500

    settings fix

--- manage.py
+++ manage.py
@@ -8,8 +8,9 @@ from django.utils import timezone
 
 if __name__ == "__main__":
 	sys.path.append('./Development/Frankenstein')
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "frank.settings")
 
-    from django.core.management import execute_from_command_line
+	os.environ.setdefault("DJANGO_SETTINGS_MODULE", "frank.settings")
 
-    execute_from_command_line(sys.argv)
+	from django.core.management import execute_from_command_line
+
+	execute_from_command_line(sys.argv)
