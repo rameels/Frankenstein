274de9716e8b00ec91d7a22c183482f236c4ef24
rsethi1 <rameel.sethi@gmail.com>
2015-06-22 03:43:11 -0500

    syspath

--- manage.py
+++ manage.py
@@ -7,6 +7,7 @@ from datetime import datetime
 from django.utils import timezone
 
 if __name__ == "__main__":
+	sys.path.append('./Development/Frankenstein')
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "frank.settings")
 
     from django.core.management import execute_from_command_line
